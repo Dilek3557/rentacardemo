f834a5172c0d496fbce1ac0ccb19e48dcf1d323d
Hüseyin Ateş <huseyin.ates@t2.com.tr>
2024-07-31 11:55:09 +0300

    Add docker-compose.yml

--- src/main/java/kodlama/io/rentacar/repository/ModelRepostitory.java
+++ src/main/java/kodlama/io/rentacar/repository/ModelRepostitory.java
@@ -3,6 +3,8 @@ package kodlama.io.rentacar.repository;
 
 import kodlama.io.rentacar.entity.ModelEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ModelRepostitory extends JpaRepository<ModelEntity, Integer> {
 }
